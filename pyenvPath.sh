7e377b9371386a217b54f7353fd4f95847d3cdb7
Kogoon <gojs712@gmail.com>
2021-06-17 07:36:05 +0000

    pyenv install and set Path

---
+++ pyenvPath.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - )"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
