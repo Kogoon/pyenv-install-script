2793e0f58668a8861b85f0ee2495080b7e8eeec7
Kogoon <gojs712@gmail.com>
2021-06-17 07:54:42 +0000

    After install pyenv, install dependencies

---
+++ afterInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update 
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
