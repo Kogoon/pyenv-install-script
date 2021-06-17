f7a6d6d43822ae871b3e4e6544f66390c26f3bbb
Kogoon <gojs712@gmail.com>
2021-06-17 07:41:43 +0000

    add command

--- pyenvPath.sh
+++ pyenvPath.sh
@@ -6,3 +6,5 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+exec $SHELL
