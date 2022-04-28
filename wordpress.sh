c813bf4cb7dec9e87a5269490d4ed781f98df1dd
Kogoon <gojs712@gmail.com>
2022-04-28 14:43:31 +0900

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -18,11 +18,14 @@ yum -y install epel-release yum-utils
 
 yum-config-manager --enable remi-php74
 
-yum -y install php
+yum install -y httpd php php-mysql php-gd php-mbstring wget unzip
 
-yum -y install php-fpm
 
-yum -y install  php-cli  php-redis  php-brotli php-intl php-gd php-gmp php-imap php-bcmath php-interbase php-json php-mbstring php-mysqlnd php-odbc php-opcache php-memcached php-tidy php-pdo php-pdo-dblib php-pear php-pgsql php-process php-pecl-apcu php-pecl-geoip php-pecl-imagick php-pecl-hrtime php-pecl-json php-pecl-memcache php-pecl-mongodb php-pecl-rar php-pecl-pq php-pecl-redis4 php-pecl-yaml php-pecl-zip
+#yum -y install php
+
+#yum -y install php-fpm
+
+#yum -y install  php-cli  php-redis  php-brotli php-intl php-gd php-gmp php-imap php-bcmath php-interbase php-json php-mbstring php-mysqlnd php-odbc php-opcache php-memcached php-tidy php-pdo php-pdo-dblib php-pear php-pgsql php-process php-pecl-apcu php-pecl-geoip php-pecl-imagick php-pecl-hrtime php-pecl-json php-pecl-memcache php-pecl-mongodb php-pecl-rar php-pecl-pq php-pecl-redis4 php-pecl-yaml php-pecl-zip
 
 
 echo "-----------------------------------------------------------------"
